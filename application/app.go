97c5a2028f9bed68f6ac02d115a78e526eee9080
Sushil-cmd-r <kandharesushil10@gmail.com>
2024-02-11 00:18:59 +0530

    graceful shutdown complete

--- application/app.go
+++ application/app.go
@@ -3,31 +3,61 @@ package application
 import (
 	"context"
 	"fmt"
+	"github.com/redis/go-redis/v9"
 	"net/http"
+	"time"
 )
 
 type App struct {
-  router http.Handler
+	router http.Handler
+	rdb    *redis.Client
 }
 
 func NewApp() *App {
-  app := &App {
-    router: loadRoutes(),
-  }
+	app := &App{
+		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
+	}
 
-  return app
-} 
+	return app
+}
 
 func (a *App) Start(ctx context.Context) error {
-  server := &http.Server{
-    Addr: ":8080",
-    Handler: a.router,
-  }
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: a.router,
+	}
 
-  err := server.ListenAndServe()
-  if err != nil {
-    return fmt.Errorf("failed to start server: %w", err)
-  }
+	err := a.rdb.Ping(ctx).Err()
+	if err != nil {
+		return fmt.Errorf("failed to connect to redis: %w", err)
+	}
 
-  return nil
-} 
+	defer func() {
+		if err = a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
+	fmt.Println("Starting server...")
+
+	ch := make(chan error, 1)
+
+	go func(ch chan error) {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}(ch)
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		fmt.Println("Shutting down gracefully...")
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		return server.Shutdown(timeout)
+	}
+}
--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
-
 	"github.com/sushil-cmd-r/order-api/application"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	app := application.NewApp()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app: ", err)
 	}
