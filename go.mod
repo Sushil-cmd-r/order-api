2e6bb95badd9f02bd474a8de655f2488d03a808d
sushil-cmd-r <kandharesushil10@gmail.com>
2024-02-10 21:53:24 +0530

    add mux router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sushil-cmd-r/order-api
 
 go 1.22.0
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/hello", basicHandler).Methods(http.MethodGet)
+
+	router.Use(loggingMiddleware)
 
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
@@ -20,4 +27,11 @@ func main() {
 
 func basicHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte("Hello World\n"))
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %q from %s\n", r.Method, r.RequestURI, r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	})
 }
